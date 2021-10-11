1510cbe4d943948e98f07660eb79f684f4feac70
kk-cc-lh <57128537+kk-cc-lh@users.noreply.github.com>
2021-10-11 16:11:26 +0800

    commit

---
+++ spock/README.md
@@ -0,0 +1,4 @@
+# 构建镜像
+docker image build -t demo-test1 -f ./spock/Dockerfile ./
+
+1、
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About1</router-link>
   </div>
   <router-view />
 </template>
@@ -12,7 +12,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #2c3e51;
 }
 
 #nav {
