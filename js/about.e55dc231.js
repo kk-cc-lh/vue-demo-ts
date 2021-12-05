f4c52f44d386fc14e2dbca6eee7eb6876925f9f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-12-05 08:29:42 +0000

    Deploy kk-cc-lh/vue-demo-ts to github.com/kk-cc-lh/vue-demo-ts.git:gh-pages

---
+++ js/about.e55dc231.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdemo_ts"]=self["webpackChunkdemo_ts"]||[]).push([[443],{735:function(n,t,e){e.r(t),e.d(t,{default:function(){return f}});var s=e(396);const u={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function c(n,t){return(0,s.wg)(),(0,s.iD)("div",u,r)}var o=e(89);const i={},d=(0,o.Z)(i,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=about.e55dc231.js.map
