882155f53cc3f96d75ce0cca058d6520b71fdf88
kk-cc-lh <57128537+kk-cc-lh@users.noreply.github.com>
2022-08-14 15:54:06 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About1</router-link>
+  <div class="main">
+    <section class="footer">
+      <div class="content">
+        <span>备案号：</span>
+        <a href="https://beian.miit.gov.cn" target="_blank">鄂ICP备2021020526号-1</a>
+      </div>
+    </section>
   </div>
-  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e51;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100vh;
+  background: url("@/assets/bg.jpg") no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  overflow: hidden;
+  .footer {
+    padding: 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .content {
+      display: inline-block;
     }
   }
 }
